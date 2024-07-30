332d14efcf3ba830396ff6c84d1a040221a0ee1c
loshaniramsha <loshanikottagoda@gmail.com>
2024-07-30 23:19:40 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Blog;
+import com.example.demo.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @GetMapping("/getAllPost")
+    public List<Blog> getAllPost(){
+        return blogRepository.findAll();
+    }
+
+
+    @PostMapping("/savePost")
+    public Blog getAllPost(@RequestBody Blog blog){
+        return blogRepository.save(blog);
+    }
+    @GetMapping("/getOnePost/{id}")
+    public String getOnePost(@PathVariable String id){
+        return id;
+    }
+}
